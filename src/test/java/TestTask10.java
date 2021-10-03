724d1baf553e7f3ad36629b2975c9b6fddbbcce5
vit314 <paulahradski@gmail.com>
2021-10-03 20:10:28 +0300

    Initial commit

---
+++ src/test/java/TestTask10.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestTask10 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpClass() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "c:/selenium/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testW () {
+
+        driver.get("https://www.w3schools.com/");
+
+        WebElement searchBtn = driver.findElement(By.id("navbtn_tutorials"));
+        searchBtn.click();
+
+        WebElement javaLink = driver.findElement(By.xpath("(//a[@class='w3-bar-item w3-button'][normalize-space()='Learn Java'])[1]"));
+        javaLink.click();
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Java Tutorial";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+
+
+    }
+
+
+}
